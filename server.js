c78a57fa24fc58a9dd00592b32093e3ced8a25b3
hyperkarmic <randy_pan@yahoo.com>
2022-06-06 20:08:56 +0100

    using objects the same way REACT uses props - only as a secondary argument at app-route level - this is also lecture3

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const express = require('express')
 const hbs = require('express-handlebars')
 
 const app = express()
-app.engine('handlebars', hbs.engine())
+app.engine('handlebars', hbs.engine({}))
 app.set('view engine', 'handlebars')
 app.set('views', './views')
 
 app.get('/', (req, res) => {
-  res.render('home')
+  res.render('home', { title: 'home page', name: 'slim shady' })
 })
 
 app.get('/about', (req, res) => {
-  res.render('about')
+  res.render('about', { title: 'about', contents: 'spleen and ideal' })
 })
 
 app.listen(3000)
