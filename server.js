86185210fd826a0cb9c96574609cc31c71c6616f
hyperkarmic <randy_pan@yahoo.com>
2022-06-08 17:30:09 +0100

    using extname:'whatever' - in hbs.engine - and changing the handlebars to the new extention works

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const express = require('express')
 const hbs = require('express-handlebars')
 
 const app = express()
-app.engine('handlebars', hbs.engine({}))
-app.set('view engine', 'handlebars')
+app.engine(
+  'hbs',
+  hbs.engine({
+    extname: '.hbs',
+  })
+)
+app.set('view engine', 'hbs')
 app.set('views', './views')
 
 app.get('/', (req, res) => {
@@ -21,6 +26,22 @@ app.get('/if', (req, res) => {
 
 app.get('/unless', (req, res) => {
   res.render('if', { content: 'if', nam: 'shasame and reproach' })
+})
+
+app.get('/each', (req, res) => {
+  res.render('each', {
+    guys: {
+      pete: 'stinks',
+      matt: 'shit actor',
+      mark: 'nonk scum',
+      ian: 'twatty',
+    },
+    lists: [
+      { items: ['cheese', 'chive', 'onion'] },
+      { items: ['tofu', 'rocket', 'shallot'] },
+      { items: ['hummous', 'paprika', 'naan'] },
+    ],
+  })
 })
 
 app.listen(3000)
