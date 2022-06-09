344988abf783a2c16c81ce3af03a74409d82e2ae
hyperkarmic <randy_pan@yahoo.com>
2022-06-09 19:10:25 +0100

    hardest part of the course.  functions can be made using a hbs.create({}) - which creates a modified form of the engine - that can use new paramters - contained within its object

--- server.js
+++ server.js
@@ -3,12 +3,26 @@ const express = require('express')
 const hbs = require('express-handlebars')
 
 const app = express()
-app.engine(
-  'hbs',
-  hbs.engine({
-    extname: '.hbs',
-  })
-)
+const bbs = hbs.create({
+  extname: '.hbs',
+  //create custom helpers
+  helpers: {
+    calculation: function (value) {
+      return value + 7
+    },
+    list: function (value, options) {
+      // value === people
+      let out = '<ul>'
+
+      for (let i = 0; i < value.length; i++) {
+        out = out + '<li>' + options.fn(value[i]) + '</li>'
+      }
+      return out + '</ul>'
+    },
+  },
+})
+
+app.engine('hbs', bbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', './views')
 
@@ -33,9 +47,23 @@ app.get('/partial', (req, res) => {
 })
 
 app.get('/lookup', (req, res) => {
-  res.render('lookup',user = {
-   user:{content:"screw you"},
-   arr:["mouser","ratter","pigeon poker"]
+  res.render(
+    'lookup',
+    (user = {
+      user: { content: 'screw you' },
+      arr: ['mouser', 'ratter', 'pigeon poker'],
+    })
+  )
+})
+
+app.get('/helper', (req, res) => {
+  res.render('helper', {
+    title: 'helper',
+    name: 'slim shady',
+    people: [
+      { firstName: 'Stephen Patrick', lastName: 'Morrissey' },
+      { firstName: 'Johnny', lastName: 'Marr' },
+    ],
   })
 })
 
